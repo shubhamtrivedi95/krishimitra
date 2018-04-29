be7fa49c96ba41412a4a4245ac4608b254256fd8
Shubham Trivedi <shubhamtrivedi95@gmail.com>
2018-04-29 13:30:16 +0530

    db update

--- krishimitra/farmer/models.py
+++ krishimitra/farmer/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class client(models.Model):
 
-    MobileNo = models.CharField(max_length=10 )
+    MobileNo = models.IntegerField()
     CustmerName=models.CharField(max_length=20)
     Enable = models.IntegerField(null=True, blank=True)
     DeviceId=models.IntegerField(unique=True)
